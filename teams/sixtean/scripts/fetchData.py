0f26f6657c6d53b41b18300b150d9f6f9829efb9
iuhisa <iuhisa1271@gmail.com>
2022-11-24 12:45:21 +0900

    added sixtean

--- teams/sixtean/scripts/fetchData.py
+++ teams/sixtean/scripts/fetchData.py
@@ -11,6 +11,26 @@ def readCSV(file_url:str) -> list:
         reader = csv.DictReader(f)
         return [row for row in reader]
 
+def readCSVwithKey(file_url:str) -> dict:
+    res = {}
+    with open(file_url, 'r') as f:
+        reader = csv.DictReader(f)
+        src = [row for row in reader]
+        for row in src:
+            key = row['key']
+            if key not in res:
+                res[key] = []
+            del row['key']
+            res[key].append(row)
+    return res
+
+def dict2list(src:dict)->list:
+    res = []
+    for key in src.keys():
+        for row in src[key]:
+            res.append([key, str(row['latitude']), str(row['longitude']), str(row['name'])])
+    return res
+
 def avePoints(points:list) -> dict:
     sum_lat = 0
     sum_long = 0
@@ -21,7 +41,24 @@ def avePoints(points:list) -> dict:
     return [sum_lat/len(points), sum_long/len(points)]
 
 #rowsの緯度経度のユークリッド距離を使って近傍の点（mag個）を結合
-def aggregatePoints(points: list, mag: int = 4) -> list:
+def aggregatePoints(points: list, mag: int = 4, is_dict:bool = False) -> list:
+    res = []
+    while len(points) >= mag:
+        points_ = []
+        points_.append(points.pop(0))
+        points.sort(key = lambda x: pow(float(points_[0]['latitude'])-float(x['latitude']),2)+pow(float(points_[0]['longitude'])-float(x['longitude']),2))
+        for i in range(mag-1):
+            points_.append(points.pop(0))
+
+        if is_dict:
+            ave = avePoints(points_)
+            res.append({'latitude':ave[0], 'longitude':ave[1], 'name':''})
+        else:
+            res.append(avePoints(points_))
+
+    return res
+
+def aggregatePointswithName(points: list, mag: int = 4) -> list:
     res = []
     while len(points) >= mag:
         points_ = []
